8d1fb78334cf091562b6e178a9a66c23ea9816ba
Pindaroso <me@pindaroso.com>
2019-03-06 17:29:37 -0500

    bug: Fixed syntax error

--- signpdf.py
+++ signpdf.py
@@ -23,11 +23,6 @@ parser.add_argument('--coords',
                     help='Coordinates to place signature. Format: PAGExXxYxWIDTHxHEIGHT. 1x200x300x125x40 means page 1, 200 units horizontally from the bottom left, 300 units vertically from the bottom left, 125 units wide, 40 units tall. Pages count starts at 1 (1-based indexing). Units are pdf-standard units (1/72 inch).')
 
 
-def _get_tmp_filename(suffix='.pdf'):
-    with tempfile.NamedTemporaryFile(suffix='.pdf') as fh:
-        return fh.name
-
-
 def sign_pdf(args):
     page_num, x1, y1, width, height = [int(a) for a in args.coords.split('x')]
     page_num -= 1
@@ -49,21 +44,22 @@ def sign_pdf(args):
 
         if i == page_num:
             # Create PDF for signature
-            sig_tmp_filename = _get_tmp_filename()
-            c = canvas.Canvas(sig_tmp_filename, pagesize=page.cropBox)
-            c.drawImage(args.signature, x1, y1, width, height, mask='auto')
-            if args.date:
-                c.drawString(x1 + width, y1,
-                             datetime.datetime.now().strftime('%Y-%m-%d'))
-            c.showPage()
-            c.save()
+            with tempfile.NamedTemporaryFile(suffix='.pdf') as fh:
+                sig_tmp_filename = fh.name
+                c = canvas.Canvas(sig_tmp_filename, pagesize=page.cropBox)
+                c.drawImage(args.signature, x1, y1, width, height, mask='auto')
+                if args.date:
+                    c.drawString(x1 + width, y1,
+                                 datetime.datetime.now().strftime('%Y-%m-%d'))
+                c.showPage()
+                c.save()
 
-            # Merge PDF in to original page
-            sig_tmp_fh = open(sig_tmp_filename, 'rb')
-            sig_tmp_pdf = PyPDF2.PdfFileReader(sig_tmp_fh)
-            sig_page = sig_tmp_pdf.getPage(0)
-            sig_page.mediaBox = page.mediaBox
-            page.mergePage(sig_page)
+                # Merge PDF in to original page
+                sig_tmp_fh = open(sig_tmp_filename, 'rb')
+                sig_tmp_pdf = PyPDF2.PdfFileReader(sig_tmp_fh)
+                sig_page = sig_tmp_pdf.getPage(0)
+                sig_page.mediaBox = page.mediaBox
+                page.mergePage(sig_page)
 
         writer.addPage(page)
 
@@ -73,13 +69,7 @@ def sign_pdf(args):
     for handle in [pdf_fh, sig_tmp_fh]:
         if handle:
             handle.close()
-    if sig_tmp_filename:
-        os.remove(sig_tmp_filename)
-
-
-def main():
-    sign_pdf(parser.parse_args())
 
 
 if __name__ == '__main__':
-    main()
+    sign_pdf(parser.parse_args())
